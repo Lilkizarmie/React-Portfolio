0288cb2a3b74b0535d20e1ef8d4a701b872b19e6
Lilkizarmie <jefferyadolor18@gmail.com>
2022-05-18 02:41:21 +0100

    completed the header description

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -18,9 +18,12 @@ import sass from '../assets/sass.png';
 import typescript from '../assets/typescript.png';
 import vue from '../assets/vue.png';
 
+import about01 from '../assets/about01.png';
 import about02 from '../assets/about02.png';
 import about03 from '../assets/about03.png';
+import about04 from '../assets/about04.png';
 
+import profile from '../assets/profile.png';
 import circle from '../assets/circle.svg';
 import logo from '../assets/logo.png';
 
@@ -52,8 +55,11 @@ export default {
   sass,
   typescript,
   vue,
+  about01,
   about02,
   about03,
+  about04,
+  profile,
   circle,
   logo,
   adidas,
